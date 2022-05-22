b7fbaf45240fa90538a63d368eb9aaf36fb12432
Yaroslav Latuhin <lyv23@icloud.com>
2022-05-22 23:02:04 +0300

    feat: Background image was added

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 const Feed: FC<IProps> = ({ title, description }) => (
 	<li className="grid w-[340px] gap-4">
-		<div className="h-[192px] w-full rounded-lg bg-darkTypo" />
+		<div className="h-[192px] w-full rounded-lg bg-[url('/images/feed/post.jpg')]" />
 		<div className="grid gap-1.5">
 			<h3 className="text-lg font-bold text-darkTypo line-clamp-1">{title}</h3>
 			<p className="text-darkTypo line-clamp-2">{description}</p>
