8d555f0bc9b0cebd490e4f1e28dc33ebfe7e1bbb
Yaroslav Latuhin <lyv23@icloud.com>
2022-06-02 03:28:22 +0300

    Feat: Formatting update

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -2,13 +2,15 @@ import { FC } from 'react';
 import { SUBSCR_BTN_TEXT, SUBSCR_HOLDER_TEXT } from '$constants';
 
 const MailForm: FC = () => (
-	<div className="flex h-[32px] md:h-[43px] max-w-[364px] md:max-w-[484px]">
+	<div className="flex h-[32px] max-w-[364px] md:h-[43px] md:max-w-[484px]">
 		<input
-			className="h-full grow border-black  px-3 md:px-4 text-xs md:text-sm font-bold text-black placeholder:text-sm placeholder:font-bold placeholder:text-black"
+			className="h-full grow border-black  px-3 text-xs font-bold text-black placeholder:text-sm placeholder:font-bold placeholder:text-black md:px-4 md:text-sm"
 			placeholder={SUBSCR_HOLDER_TEXT}
 			type="text"
 		/>
-		<button className="h-full bg-grey px-3 md:px-4 md:text-lg font-bold text-white">{SUBSCR_BTN_TEXT}</button>
+		<button className="h-full bg-grey px-3 font-bold text-white md:px-4 md:text-lg">
+			{SUBSCR_BTN_TEXT}
+		</button>
 	</div>
 );
 
--- src/containers/Subscription/Subscription.tsx
+++ src/containers/Subscription/Subscription.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 const Subscription: FC<IProps> = ({ title, text }) => (
 	<div className="bg-subscriptionBanner bg-cover">
-		<div className="xl:px-20 container grid pt-12 pb-18 md:pt-16 md:pb-24">
+		<div className="container grid pt-12 pb-18 md:pt-16 md:pb-24 xl:px-20">
 			<div className="grid grid-flow-row gap-7 md:w-9/12 md:gap-10 xl:w-2/4">
 				<div className="grid grid-flow-row gap-3 md:gap-4">
 					<h2 className="text-3xl font-bold text-white md:text-4xl">{title}</h2>
